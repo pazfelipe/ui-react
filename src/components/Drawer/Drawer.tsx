62956adbba94077600a02702087df90feb14f8bb
Felipe Paz <xxfelipepaz@gmail.com>
2022-03-12 01:22:41 -0300

    feat: import new component for open drawer

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import BackgroundDrawer from './BackgroundDrawer';
 import StyledDrawer from './styled';
 
 const Drawer = () => {
@@ -7,28 +8,41 @@ const Drawer = () => {
   const isClose = useSelector((state: { drawer: { close: boolean; }; }) => state.drawer.close);
   const theme = useSelector((state: { theme: { dark: boolean; }; }) => state.theme);
 
+  const closeDrawer = () => {
+    dispatch({
+      type: 'CLOSE_DRAWER'
+    });
+  };
+
   const isMobile = window.innerWidth < 768;
 
   return (
-    <StyledDrawer
-      className={
-        [
-          isClose ? 'close' : '',
-          theme.dark ? 'dark-theme' : '',
-          isMobile ? 'mobile' : '',
-          !isClose && isMobile ? 'mobile-open' : '',
-        ].join(' ')
-      }
-    >
-      Drawer
-      <button
-        onClick={ () => dispatch({
-          type: 'CLOSE_DRAWER'
-        }) }
+    <>
+      <StyledDrawer
+        className={
+          [
+            isClose ? 'close' : '',
+            theme.dark ? 'dark-theme' : '',
+            isMobile ? 'mobile' : '',
+            !isClose && isMobile ? 'mobile-open' : '',
+          ].join(' ')
+        }
       >
-        Close
-      </button>
-    </StyledDrawer>
+        Drawer
+        <button
+          onClick={ closeDrawer }
+        >
+          Close
+        </button>
+      </StyledDrawer>
+
+      <BackgroundDrawer
+        isClose={ isClose }
+        isMobile={ isMobile }
+        closeDrawer={ closeDrawer }
+      />
+
+    </>
   );
 
 };
