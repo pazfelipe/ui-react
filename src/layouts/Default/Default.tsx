d305a555efea01e646a2c8b326d853909d2635a5
Felipe Paz <xxfelipepaz@gmail.com>
2022-03-12 02:35:46 -0300

    feat: add gesture to toggle drawer on mobile device

--- src/layouts/Default/Default.tsx
+++ src/layouts/Default/Default.tsx
@@ -1,7 +1,7 @@
 import AppBar from 'components/AppBar/AppBar';
 import Drawer from 'components/Drawer/Drawer';
 import MainContainer from 'components/MainContainer/MainContainer';
-import { useLayoutEffect } from 'react';
+import React, { useCallback, useEffect, useLayoutEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ChildrenProps } from '../../interfaces/ChildrenProps';
 import StyledLayout from './styled';
@@ -12,11 +12,54 @@ const DefaultLayout = ({ children }: ChildrenProps) => {
   const theme = useSelector((state: { theme: { dark: boolean; }; }) => state.theme);
   const isDrawerClosed = useSelector((state: { drawer: { close: boolean; }; }) => state.drawer.close);
 
+  const elementRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+
   const handleClick = () => {
     dispatch({
       type: 'TOGGLE_THEME',
     });
   };
+
+  const handleTouchStart = (event: TouchEvent) => {
+    touchstartX.current = event.changedTouches[ 0 ].screenX;
+  };
+
+
+  let touchstartX = useRef(0);
+  let touchendX = useRef(0);
+
+  const handleGesture = useCallback(() => {
+    if (touchendX.current + 50 < touchstartX.current) {
+      dispatch({
+        type: 'CLOSE_DRAWER',
+      });
+    };
+    if (touchendX.current > touchstartX.current + 50) {
+      dispatch({
+        type: 'OPEN_DRAWER',
+      });
+    };
+  }, [ dispatch ]);
+
+  const handleTouchEnd = useCallback((event: TouchEvent) => {
+    touchendX.current = event.changedTouches[ 0 ].screenX;
+    handleGesture();
+  }, [ handleGesture ]);
+
+  useEffect(() => {
+    const ef = elementRef?.current;
+    if (elementRef) {
+
+      ef?.addEventListener('touchstart', handleTouchStart);
+
+      ef?.addEventListener('touchend', handleTouchEnd);
+    }
+
+    return () => {
+      ef.removeEventListener('touchstart', handleTouchStart);
+      ef.removeEventListener('touchend', handleTouchEnd);
+    };
+  }, [ handleTouchEnd ]);
 
   useLayoutEffect(() => {
     if (window.innerWidth < 768) {
@@ -29,6 +72,7 @@ const DefaultLayout = ({ children }: ChildrenProps) => {
   return (
     <StyledLayout
       className={ theme.dark ? 'dark-theme' : '' }
+      ref={ elementRef }
     >
       <Drawer />
       <div style={ { width: '100%', height: '100vh' } }>
